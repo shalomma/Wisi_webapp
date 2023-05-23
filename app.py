8be5e80c8475f375550028b4e8054899327b7e29
Maayan Shalom <sha.maayan@gmail.com>
2023-05-23 23:19:43 +0300

    cols and voice checkbox

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ from io import BytesIO
 import streamlit as st
 from streamlit_chat import message
 from elevenlabs import generate, set_api_key
+import elevenlabs.api.error
 import streamlit.components.v1 as components
 
 set_api_key(os.getenv("ELEVENLABS_API_KEY"))
 
 # Setting page title and header
-st.set_page_config(page_title="Wisi", page_icon=":robot_face:")
-st.markdown("<h1 style='text-align: center;'>Wisi - your social tutor 🦉</h1>", unsafe_allow_html=True)
+st.set_page_config(page_title="Wisi", page_icon=":robot_face:", layout="wide")
 
 # Initialise system instructions
 with open('system.txt', 'r') as f:
@@ -29,20 +29,23 @@ if 'messages' not in st.session_state:
 
 
 def update_system_instruction():
-    st.session_state['messages'][0] = {"role": "system", "content": st.session_state.text}
+    st.session_state['messages'][0] = {"role": "system", "content": st.session_state.system_instruction}
 
 
-st.sidebar.title('')
-st.sidebar.text_area(label='System Instruction', value=system_instruction,
-                     key='text', on_change=update_system_instruction)
-clear_button = st.sidebar.button("Clear Conversation", key="clear")
+col1, col2 = st.columns(2, gap='medium')
 
-# reset everything
-if clear_button:
-    st.session_state['generated'] = []
-    st.session_state['past'] = []
-    st.session_state['messages'] = [{"role": "system", "content": system_instruction}]
-    st.session_state['number_tokens'] = []
+with col1:
+    st.text_area(label='System Instruction', value=system_instruction, height=350,
+                 key='system_instruction', on_change=update_system_instruction)
+    clear_button = st.button("Clear Conversation", key="clear")
+    voice = st.checkbox('Voice')
+
+    # reset everything
+    if clear_button:
+        st.session_state['generated'] = []
+        st.session_state['past'] = []
+        st.session_state['messages'] = [{"role": "system", "content": system_instruction}]
+        st.session_state['number_tokens'] = []
 
 
 # generate a response
@@ -73,34 +76,40 @@ def audio_to_html(audio_bytes):
 
 
 def text_to_speech_elevenlabs(response):
-    audio_stream = generate(
-        text=response,
-        voice="Bella",
-        model="eleven_monolingual_v1"
-    )
-    audio_html = audio_to_html(audio_stream)
+    try:
+        audio_stream = generate(
+            text=response,
+            voice="Bella",
+            model="eleven_monolingual_v1"
+        )
+        audio_html = audio_to_html(audio_stream)
+    except elevenlabs.api.error.APIError:
+        audio_html = ''
     return audio_html
 
 
-# container for chat history
-response_container = st.container()
-# container for text box
-container = st.container()
+with col2:
+    st.markdown("<h1 style='text-align: center;'>Wisi - your social tutor 🦉</h1>", unsafe_allow_html=True)
+    # container for chat history
+    response_container = st.container()
+    # container for text box
+    container = st.container()
 
-with container:
-    with st.form(key='my_form', clear_on_submit=True):
-        user_input = st.text_area("You:", key='input', height=100)
-        submit_button = st.form_submit_button(label='Send')
+    with container:
+        with st.form(key='my_form', clear_on_submit=True):
+            user_input = st.text_input("You:", key='input')
+            submit_button = st.form_submit_button(label='Send')
 
-    if submit_button and user_input:
-        output = generate_response(user_input)
-        st.session_state['past'].append(user_input)
-        st.session_state['generated'].append(output)
-        audio = text_to_speech_elevenlabs(output)
-        components.html(audio, height=0)
+        if submit_button and user_input:
+            output = generate_response(user_input)
+            st.session_state['past'].append(user_input)
+            st.session_state['generated'].append(output)
+            if voice:
+                audio = text_to_speech_elevenlabs(output)
+                components.html(audio, height=0)
 
-if st.session_state['generated']:
-    with response_container:
-        for i in range(len(st.session_state['generated'])):
-            message(st.session_state["past"][i], is_user=True, key=str(i) + '_user')
-            message(st.session_state["generated"][i], key=str(i))
+    if st.session_state['generated']:
+        with response_container:
+            for i in range(len(st.session_state['generated'])):
+                message(st.session_state["past"][i], is_user=True, key=str(i) + '_user')
+                message(st.session_state["generated"][i], key=str(i))
